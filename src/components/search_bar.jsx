953e4101909737e8747d31c695c205fbce5a41e5
Gavin Garza <48096041+RenGVZ@users.noreply.github.com>
2020-09-10 23:55:36 +0900

    added search bar styling

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import '../styles/search_bar.css'
+import '../styles/search_bar.scss'
 
 function SearchBar() {
   return (
     <div className="form__group field">
       <input type="input" className="form__field" placeholder="Name" name="name" id='name' required />
-      <label for="name" class="form__label">Name</label>
+      <label htmlFor="name" className="form__label">Name</label>
     </div>
   )
 }
