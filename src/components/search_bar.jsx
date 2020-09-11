eb1531859f2d54644397a2d2da58bfa976e9e5bf
Gavin Garza <48096041+RenGVZ@users.noreply.github.com>
2020-09-11 20:32:15 +0900

    fixed jsx and added click button to search bar

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -3,9 +3,10 @@ import '../styles/search_bar.scss'
 
 function SearchBar() {
   return (
-    <div className="form__group field">
-      <input type="input" className="form__field" placeholder="Name" name="name" id='name' required />
-      <label htmlFor="name" className="form__label">Name</label>
+    <div className="form-group">
+        <span>Location</span>
+        <input className="form-field" type="text" placeholder="Type a city..."/>
+        <button>Click</button>
     </div>
   )
 }
